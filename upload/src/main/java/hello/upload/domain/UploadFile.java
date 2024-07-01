824f9e78e727abb2ec23baad2584b398ed601e19
april0391 <april0391@naver.com>
2024-07-01 16:29:45 +0900

    file upload

---
+++ upload/src/main/java/hello/upload/domain/UploadFile.java
@@ -0,0 +1,16 @@
+package hello.upload.domain;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+public class UploadFile {
+
+    private String uploadFileName;
+    private String storeFileName;
+
+    public UploadFile(String uploadFileName, String storeFileName) {
+        this.uploadFileName = uploadFileName;
+        this.storeFileName = storeFileName;
+    }
+}
