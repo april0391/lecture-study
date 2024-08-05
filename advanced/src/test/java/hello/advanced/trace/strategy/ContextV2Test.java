3ad0f4853a3c9919af40b7f42558c4956a1ebc6b
april0391 <april0391@naver.com>
2024-08-05 17:55:13 +0900

    threadlocal, template method pattern, strategy pattern, template callback pattern

---
+++ advanced/src/test/java/hello/advanced/trace/strategy/ContextV2Test.java
@@ -0,0 +1,45 @@
+package hello.advanced.trace.strategy;
+
+import hello.advanced.trace.strategy.code.strategy.ContextV2;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV2Test {
+
+    /**
+     * 전략 패턴 적용
+     */
+    @Test
+    void strategyV1() {
+        ContextV2 context = new ContextV2();
+        context.execute(new StrategyLogic1());
+        context.execute(new StrategyLogic2());
+        context.execute(() -> log.info("람다"));
+        context.execute(() -> {
+            log.info("람다");
+            log.info("로직 여러줄");
+        });
+    }
+
+    @Test
+    void strategyV2() {
+        ContextV2 context = new ContextV2();
+        context.execute(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행 익명 내부클래스");
+            }
+        });
+    }
+
+    @Test
+    void strategyV3() {
+        ContextV2 context = new ContextV2();
+        context.execute(() -> log.info("비즈니스 로직1 실행 람다"));
+        context.execute(() -> log.info("비즈니스 로직2 실행 람다"));
+    }
+}
