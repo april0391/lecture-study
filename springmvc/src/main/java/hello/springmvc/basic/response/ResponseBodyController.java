7b91e8a7ebf5382740e5e9a51944cc2894a4f559
april0391 <april0391@naver.com>
2024-10-09 23:45:11 +0900

    http response, http message converter

--- springmvc/src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
+++ springmvc/src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -22,31 +22,26 @@ public class ResponseBodyController {
     }
 
     @GetMapping("/response-body-string-v2")
-    public ResponseEntity<String> responseBodyV2() {
-        return new ResponseEntity<>("ok", HttpStatus.OK);
+    public ResponseEntity<String> responseBodyV2(HttpServletResponse response) {
+        return ResponseEntity.ok("ok");
     }
 
     @ResponseBody
     @GetMapping("/response-body-string-v3")
-    public String responseBodyV3() {
+    public String responseBodyV3(HttpServletResponse response) {
         return "ok";
     }
 
     @GetMapping("/response-body-json-v1")
     public ResponseEntity<HelloData> responseBodyJsonV1() {
-        HelloData helloData = new HelloData();
-        helloData.setUsername("userA");
-        helloData.setAge(20);
-        return new ResponseEntity<>(helloData, HttpStatus.OK);
+        return ResponseEntity.ok(new HelloData("hello", 10));
     }
 
-    @ResponseStatus(HttpStatus.OK)
+    @ResponseStatus(HttpStatus.ACCEPTED)
     @ResponseBody
     @GetMapping("/response-body-json-v2")
     public HelloData responseBodyJsonV2() {
-        HelloData helloData = new HelloData();
-        helloData.setUsername("userA");
-        helloData.setAge(20);
-        return helloData;
+        return new HelloData("hello", 10);
     }
+
 }
