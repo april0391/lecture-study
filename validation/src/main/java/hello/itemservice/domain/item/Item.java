3801039e63077695b9929cc23ff0d4a022052021
april0391 <april0391@naver.com>
2024-10-15 18:20:15 +0900

    bean validation

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,17 +1,30 @@
 package hello.itemservice.domain.item;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "가격 * 수량이 10000원 넘게 입력해주세요.")
 public class Item {
 
+//    @NotNull(groups = UpdateCheck.class)
     private Long id;
-    private String itemName;
-    private Integer price;
-    private Integer quantity;
 
-    public Item() {
-    }
+//    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
+    private String itemName;
+
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
+    private Integer price;
+
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Max(value = 9999, groups = SaveCheck.class)
+    private Integer quantity;
 
     public Item(String itemName, Integer price, Integer quantity) {
         this.itemName = itemName;
