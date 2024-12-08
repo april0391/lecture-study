81dd60b65b0d321bbceaa3203c0197a8c858c7b8
april0391 <april0391@naver.com>
2024-12-08 18:26:07 +0900

    actuator, micrometer

--- mastering-spring/boot/actuator/src/main/java/hello/ActuatorApplication.java
+++ mastering-spring/boot/actuator/src/main/java/hello/ActuatorApplication.java
@@ -1,28 +1,24 @@
 package hello;
 
-import hello.order.guage.StockConfigV1;
-import hello.order.guage.StockConfigV2;
-import hello.order.v0.OrderConfigV0;
-import hello.order.v1.OrderConfigV1;
-import hello.order.v2.OrderConfigV2;
-import hello.order.v3.OrderConfigV3;
-import hello.order.v4.OrderConfigV4;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 
-//@Import(OrderConfigV0.class)
-//@Import(OrderConfigV1.class)
-//@Import(OrderConfigV2.class)
-//@Import(OrderConfigV3.class)
-@Import({OrderConfigV4.class, StockConfigV2.class})
-@SpringBootApplication(scanBasePackages = "hello.controller")
+@SpringBootApplication
 public class ActuatorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ActuatorApplication.class, args);
+    }
+
+    @Bean
+    ApplicationRunner applicationRunner(ApplicationContext ac) {
+        int beanDefinitionCount = ac.getBeanDefinitionCount();
+        System.out.println("beanDefinitionCount = " + beanDefinitionCount);
+        return null;
     }
 
     @Bean
