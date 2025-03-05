5177f38eeb8d036fb78cd9bf1e257d452db04203
april0391 <april0391@naver.com>
2025-03-05 23:11:30 +0900

    es6

--- understanding-typescript/src/app.ts
+++ understanding-typescript/src/app.ts
@@ -1,16 +1,60 @@
-let userInput: unknown;
-let userName: string;
+// const userName = 'Max';
+// // userName = 'Maximilian';
+// let age = 30;
 
-userInput = 5;
-userInput = "Max";
+// age = 29;
 
-if (typeof userInput === "string") {
-  userName = userInput;
-}
+// function add(a: number, b: number) {
+//   let result;
+//   result = a + b;
+//   return result;
+// }
 
-function generateError(message: string, code: number): never {
-  throw { message: message, errorCode: code };
-}
+// if (age > 20) {
+//   let isOld = true;
+// }
 
-const result = generateError("An error occurred!", 500);
-console.log(result);
+// console.log(isOld);
+
+// console.log(result);
+
+// const add = (a: number, b: number = 1) => a + b;
+
+// const printOutput: (a: number | string) => void = output => console.log(output);
+
+// const button = document.querySelector('button');
+
+// if (button) {
+//   button.addEventListener('click', event => console.log(event));
+// }
+
+// printOutput(add(5));
+
+const hobbies = ["Sports", "Cooking"];
+const activeHobbies = ["Hiking"];
+
+activeHobbies.push(...hobbies);
+
+const person = {
+  firstName: "Max",
+  age: 30,
+};
+
+const copiedPerson = { ...person };
+
+const add = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
+
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
+
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+
+console.log(hobbies, hobby1, hobby2);
+
+const { firstName: userName, age } = person;
+
+console.log(userName, age, person);
