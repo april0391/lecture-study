5207fc9dcd82d56347660ed6a0053ee4d0ae76a5
april0391 <april0391@naver.com>
2025-03-31 04:38:28 +0900

    responsive nav, table

--- shadcnui-nextjs/app/dashboard/components/main-menu.tsx
+++ shadcnui-nextjs/app/dashboard/components/main-menu.tsx
@@ -3,11 +3,14 @@ import MenuItem from "./menu-item";
 import MenuTitle from "./menu-title";
 import Link from "next/link";
 import { LightDarkToggle } from "@/components/light-dark-toggle";
+import { cn } from "@/lib/utils";
 
-export default function MainMenu() {
+export default function MainMenu({ className }: { className?: string }) {
   return (
-    <nav className="bg-muted flex flex-col overflow-auto p-4">
-      <header className="border-b border-b-zinc-300 pb-4 dark:border-b-black">
+    <nav
+      className={cn("md:bg-muted flex flex-col overflow-auto p-4", className)}
+    >
+      <header className="hidden border-b border-b-zinc-300 pb-4 md:block dark:border-b-black">
         <MenuTitle />
       </header>
       <ul className="grow py-4">
--- shadcnui-nextjs/app/dashboard/layout.tsx
+++ shadcnui-nextjs/app/dashboard/layout.tsx
@@ -1,11 +1,41 @@
+"use client";
+
+import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
 import MainMenu from "./components/main-menu";
+import MenuTitle from "./components/menu-title";
+import { MenuIcon } from "lucide-react";
+import { useMediaQuery } from "@/hooks/use-media-query";
+import { useState } from "react";
 
 export default function DashboardLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
   return (
-    <div className="grid h-screen grid-cols-[250px_1fr]">
-      <MainMenu />
+    <div className="h-screen md:grid md:grid-cols-[250px_1fr]">
+      {isDesktop ? (
+        <MainMenu className="hidden md:flex" />
+      ) : (
+        <div className="bg-background border-border sticky top-0 left-0 flex justify-between border-b p-4 md:hidden">
+          <MenuTitle />
+          <Drawer
+            direction="right"
+            open={mobileMenuOpen}
+            onClose={() => setMobileMenuOpen(false)}
+            onOpenChange={(open) => setMobileMenuOpen(open)}
+          >
+            <DrawerTrigger>
+              <MenuIcon />
+            </DrawerTrigger>
+            <DrawerContent>
+              <MainMenu />
+            </DrawerContent>
+          </Drawer>
+        </div>
+      )}
       <div className="overflow-auto px-4 py-2">
         <h1 className="pb-4">Welcome back, Tom!</h1>
         {children}
