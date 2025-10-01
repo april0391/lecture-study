56c421a4cca90023a1c982e0a6b3a30024c0638c
april <april0391.dev@gmail.com>
2025-10-02 04:02:16 +0900

    mvvm, retrofit

---
+++ android/best-android-kotlin/InheritanceApp/app/src/main/java/com/example/inheritanceapp/Start.kt
@@ -0,0 +1,13 @@
+import com.example.inheritanceapp.BaseClass
+import com.example.inheritanceapp.Offspring
+import com.example.inheritanceapp.Secondary
+import com.example.inheritanceapp.Tertiary
+
+fun main() {
+    val frank = Offspring()
+    frank.archery()
+    frank.role()
+    frank.coreValues()
+    frank.sing()
+}
+
